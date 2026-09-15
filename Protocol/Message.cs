63522d25ebafdcc33104c5c6cd2313b2210863fa
Playm0r3 <warren.macabre@proton.me>
2026-09-15 19:37:59 +0200

    [Add] Protocol method

---
+++ Protocol/Message.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wm
+{
+    public class Message : Protocol
+    {
+        public Message() { }
+
+        public byte[] Encode(string message)
+        {
+            byte[] payload = Encoding.UTF8.GetBytes(message);
+            byte[] messageLength = BitConverter.GetBytes(payload.Length);
+
+            byte[] packet = new byte[4 + payload.Length];
+
+            Buffer.BlockCopy(messageLength, 0, packet, 0, 4);
+            Buffer.BlockCopy(payload, 0, packet, 4, payload.Length);
+
+            return packet;
+        }
+
+        public string Decode(byte[] packet)
+        {
+            int messageLength = BitConverter.ToInt32(packet, 0);
+            byte[] payload = new byte[messageLength];
+            Buffer.BlockCopy(packet, 4, payload, 0, messageLength);
+            return Encoding.UTF8.GetString(payload);
+        }
+    }
+}
---
+++ Protocol/Protocol.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Sockets;
+
+namespace wm
+{
+    public abstract class Protocol
+    {
+
+        public static Protocol CreateProtocol(ProtocolType protocolType)
+        {
+            return protocolType switch
+            {
+                ProtocolType.Message => new Message(),
+                _ => throw new ArgumentException("Invalid protocol type"),
+            };
+        }
+
+    }
+}
